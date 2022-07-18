a77fca7ac3ba85abfbf11349cfcb6a7644806dc9
Joshua Steed <joshuasteed123@gmail.com>
2022-07-18 16:23:19 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+<Navbar/>
+  <router-view/>
+</template>
+<script>
+import Navbar from "./components/Navbar.vue"
+export default {
+  name: "App",
+  components: { Navbar, },
+}
+</script>
+<style>
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css");
+/* FONTS */
+@import url('https://fonts.googleapis.com/css2?family=Palanquin:wght@200;400;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+/* font-family: 'Palanquin', sans-serif;
+font-family: 'Roboto', sans-serif; */
+/* CSS RESET */
+html {
+  box-sizing: border-box;
+  font-size: 16px;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+  margin: 0;
+  padding: 0;
+  font-weight: normal;
+}
+
+ol, ul {
+  list-style: none;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+/* COLOR SCHEME */
+:root {
+  /* backgrounds */
+  --bg-color: #101010;
+  --dgrey: #1d1d1d; 
+   /* highlights */
+  --green: #1DB954;
+  /* for text */
+  --lgrey: #8f8f8f;
+  --white: #f1f1f1;
+
+
+}
+
+::-webkit-scrollbar-track
+{
+	-webkit-box-shadow: inset 0 0 6px var(--bg-color);
+	border-radius: 10px;
+	background-color: var(--bg-color);
+}
+
+::-webkit-scrollbar
+{
+	width: 12px;
+	background-color: var(--bg-color);
+}
+
+::-webkit-scrollbar-thumb
+{
+	border-radius: 10px;
+	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+	background-color: var(--lgrey);
+}
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav>
+    <router-link to="/">HOME</router-link>
+    <router-link to="/portfolio">FOLIO</router-link>
+    <router-link to="/contact">CONTACT</router-link>
+  </nav>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+nav a{
+  text-decoration: none;
+  color: inherit;
+}
+    nav{
+      background: var(--bg-color);
+      color: white;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      column-gap: 10px;
+      font-family: 'Roboto', sans-serif;
+      font-weight: 100;
+      font-size: 13px;
+      padding: 20px;
+      border-bottom: 1px solid var(--dgrey);
+    }
+
+</style>
